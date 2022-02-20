5098ad3c06eb028dccac807bb129f1a1746a0204
Code Sandbox <hello@codesandbox.io>
2022-02-20 21:04:03 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.cc7debfb3a3cdd710f3ec346a3e7db0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c5e9320417eaa6798678939f6febbd",
+    "url": "https://abdulahhamad.github.io/csb-cs376i/index.html"
+  },
+  {
+    "revision": "9780d0f354a7f6bffc8c",
+    "url": "https://abdulahhamad.github.io/csb-cs376i/static/css/main.d47e8bd2.chunk.css"
+  },
+  {
+    "revision": "a0513bf2ea8e6e83467c",
+    "url": "https://abdulahhamad.github.io/csb-cs376i/static/js/2.8bc12123.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://abdulahhamad.github.io/csb-cs376i/static/js/2.8bc12123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9780d0f354a7f6bffc8c",
+    "url": "https://abdulahhamad.github.io/csb-cs376i/static/js/main.8a6a7964.chunk.js"
+  },
+  {
+    "revision": "01d9182cb4b0adeb2c74",
+    "url": "https://abdulahhamad.github.io/csb-cs376i/static/js/runtime-main.3f1a1f9c.js"
+  }
+]);
